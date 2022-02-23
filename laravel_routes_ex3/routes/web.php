8223f17ef1fd8fd5346c85c6c86068f6870e2380
N00b13-M1 <michael.cornelis@gmail.com>
2022-02-23 09:04:06 +0100

    Exo3

--- laravel_routes_ex3/routes/web.php
+++ laravel_routes_ex3/routes/web.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 
 use Illuminate\Support\Facades\Route;
@@ -27,3 +36,7 @@ Route::get('/{var1}/{var2}', function ($var1,$var2) {
 
 
 });
+
+?>
+</body>
+</html>
